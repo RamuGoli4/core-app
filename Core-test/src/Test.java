326a4806a3cd575a804925830926c3188a000b30
ARP <ARP@Rama>
2022-01-28 01:49:46 +0530

    IC4

--- Core-test/src/Test.java
+++ Core-test/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Testing1");
 		System.out.println("Added Develop branch");
+		System.out.println("Added Feature2 branch");
 	}
 
 }
