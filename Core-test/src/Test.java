d920a2ea4249041773e08a297b7a505b12193159
ARP <ARP@Rama>
2022-01-28 02:07:55 +0530

    IC8

--- Core-test/src/Test.java
+++ Core-test/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 		System.out.println("Testing1");
 		System.out.println("Added Develop branch");
 		System.out.println("Added Feature1 Branch");
-		System.out.println("Added Feature1 Branch Again");
+		System.out.println("Added Feature1 Branch Again.");
 	}
 
 }
